83c237da21545e3dc110ce3b41f29f4ee240b31f
antonybogun <antonybogun@gmail.com>
2016-07-18 00:23:34 -0400

    slotmachine image to scene added

--- Scripts/scenes/slotmachine.js
+++ Scripts/scenes/slotmachine.js
@@ -15,11 +15,10 @@ var scenes;
         // PUBLIC METHODS +++++++++++++++++++++
         // Start Method
         SlotMachine.prototype.start = function () {
-            // add Intro Image
+            // add Background Image to the scene
             //add the SLOT MACHINE label to the MENU scene
-            this._slotMachineLabel = new objects.Label("GAME GOES HERE", "60px Consolas", "#000000", config.Screen.CENTER_X, config.
-                Screen.CENTER_Y);
-            this.addChild(this._slotMachineLabel);
+            this._backgroundImage = new createjs.Bitmap(assets.getResult("SlotMachine"));
+            this.addChild(this._backgroundImage);
             // add the START button to the MENU scene
             // add this scene to the global stage container
             stage.addChild(this);
--- Scripts/scenes/slotmachine.ts
+++ Scripts/scenes/slotmachine.ts
@@ -2,7 +2,8 @@
 module scenes {
     export class SlotMachine extends objects.Scene {
         //PRIVATE INSTANCE VARIABLES ++++++++++++
-        private _slotMachineLabel:objects.Label;
+        private _backgroundImage:createjs.Bitmap;
+
         // CONSTRUCTOR ++++++++++++++++++++++
         constructor() {
             super();
@@ -12,18 +13,11 @@ module scenes {
         
         // Start Method
         public start(): void {
-            // add Intro Image
+            // add Background Image to the scene
 
             //add the SLOT MACHINE label to the MENU scene
-
-            this._slotMachineLabel = new objects.Label(
-                "GAME GOES HERE",
-                "60px Consolas",
-                "#000000",
-                config.Screen.CENTER_X,config.
-                Screen.CENTER_Y);
-           this.addChild(this._slotMachineLabel); 
-
+            this._backgroundImage = new createjs.Bitmap(assets.getResult("SlotMachine"));
+            this.addChild(this._backgroundImage);
             // add the START button to the MENU scene
             // add this scene to the global stage container
             stage.addChild(this);
